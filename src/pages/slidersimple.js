aaf9470e4766b45dd162038a31908f61e3136d1d
Roman BAD <57302328+romanoz33@users.noreply.github.com>
2021-03-12 18:43:45 +0300

    Changes

--- src/pages/slidersimple.js
+++ src/pages/slidersimple.js
@@ -3,7 +3,6 @@ import theme from "theme";
 import { Theme, Link, Text, Box } from "@quarkly/widgets";
 import { Helmet } from "react-helmet";
 import { GlobalQuarklyPageStyles } from "global-page-styles";
-import * as Components from "components";
 export default (() => {
 	return <Theme theme={theme}>
 		<GlobalQuarklyPageStyles pageUrl={"slidersimple"} />
@@ -20,7 +19,6 @@ export default (() => {
 					Slider Simple
 				</Text>
 			</Box>
-			<Components.SliderSimple />
 			<Box quarkly-title="Props" height="auto" min-height="20px">
 				<Text font="600 24px sans-serif" quarkly-title="Title">
 					Пропсы:
